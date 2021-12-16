bcc30809c623a50b5d72f57b348bb61d721ed391
jhaakma <jchaakma@gmail.com>
2021-12-17 01:43:17 +1300

    Block quick menu, refactor entire mod

--- Data Files/MWSE/mods/mer/NoCombatMenu/main.lua
+++ Data Files/MWSE/mods/mer/NoCombatMenu/main.lua
@@ -1,103 +1,101 @@
 
-local configPath = "noCombatMenu"
+local CONFIG_PATH = "noCombatMenu"
+local IN_COMBAT_MESSAGE = "You are in combat."
+local DEVICE_KEYBOARD = 0
+local DEVICE_MOUSE = 1
+local BUTTONS_TO_BLOCK = {
+    tes3.keybind.menuMode,
+    tes3.keybind.quickMenu,
+}
+local HOSTILE_OBJECT_TYPES = {
+    [tes3.objectType.npc] = true,
+    [tes3.objectType.creature] = true,
+}
 
-local config = mwse.loadConfig(configPath)
-if (config == nil) then
-	config = { 
-        enable = true, 
-        safeDistance = 1000
-    }
+local config = mwse.loadConfig(CONFIG_PATH, {
+    enable = true,
+    safeDistance = 1000,
+    logLevel = "INFO"
+})
+
+local logger = require("mer.NoCombatMenu.logger").new{
+    name = "No Combat Menu",
+    logLevel = config.logLevel
+}
+
+
+local function getDeviceName(device)
+    if device == DEVICE_KEYBOARD then
+        return "Keyboard"
+    elseif device == DEVICE_MOUSE then
+        return "Mouse"
+    end
+    logger:error("Unknown device: " .. device)
+    return "Unknown"
 end
 
-local menuKeyCode
-local substituteKeyCode = 5000
-local menuInputMap
-local inCombat
-
-local function initialize()
-    menuInputMap = tes3.worldController.inputController.inputMaps[19]
+local function showInCombatMessage()
+    tes3.messageBox(IN_COMBAT_MESSAGE)
 end
 
-event.register(tes3.event.initialized, initialize)
-
-local function checkInCombat(e)
-    inCombat = true
-    --check config
-    if not config.enable then
-        inCombat = false
+local function isHostile(enemy)
+    if not enemy.mobile then return end
+    if not HOSTILE_OBJECT_TYPES[enemy.baseObject.objectType] then return end
+    --Check enemy's hostile actors for the player
+    local playerInHostileList
+    for actor in tes3.iterate(enemy.mobile.hostileActors) do
+        if actor.reference == tes3.player then
+            playerInHostileList = true
+            break
+        end
     end
-    --check menu
-    if tes3.menuMode() then
-        inCombat = false
+    if enemy.mobile.inCombat and playerInHostileList then
+        return true
     end
+    return false
+end
 
+local function getEnemyDistance(enemy)
+    return tes3.player.mobile.position:distance(enemy.position)
+end
+
+local function getDistanceOfClosestEnemy()
     --Find nearest enemy
-    local minDistance = config.safeDistance
-    local cell = tes3.getPlayerCell()
-
-    local function checkHostileDistance(enemy)
-        --Check enemy's hostile actors for the player
-        local playerInHostileList
-        for actor in tes3.iterate(enemy.mobile.hostileActors) do
-            if actor.reference == tes3.player then
-                playerInHostileList = true
-                break
+    local minDistance = math.huge
+    for _, cell in ipairs(tes3.getActiveCells()) do
+        for objectType, _ in pairs(HOSTILE_OBJECT_TYPES) do
+            for enemy in cell:iterateReferences(objectType) do
+                if isHostile(enemy) then
+                    local hostileDistance = getEnemyDistance(enemy)
+                    minDistance = math.min(minDistance, hostileDistance)
+                end
             end
         end
-        if enemy.mobile.inCombat and playerInHostileList then
-            local thisDistance = tes3.mobilePlayer.position:distance(enemy.position) 
-            minDistance = minDistance < thisDistance and minDistance or thisDistance
-        end
     end
+    return minDistance
+end
 
-    if config.enable then 
-        for enemy in cell:iterateReferences(tes3.objectType.creature) do
-            checkHostileDistance(enemy)
-        end
-        for enemy in cell:iterateReferences(tes3.objectType.npc) do
-            checkHostileDistance(enemy)
-        end
-    end
-
+local function checkInCombat()
+    if not config.enable then return false end
+    local inCombat = true
+    local minDistance = getDistanceOfClosestEnemy()
     if minDistance >= config.safeDistance then
         inCombat = false
     end
-
-    if inCombat then
-        tes3ui.leaveMenuMode(tes3ui.registerID("MenuMulti"))
-        if menuInputMap.code ~= substituteKeyCode then
-            --save menu key and replace with fake code
-            menuKeyCode = menuInputMap.code 
-            menuInputMap.code = substituteKeyCode
-        end
-    else
-        --enable menu out of combat
-        if menuInputMap.code == substituteKeyCode then
-            menuInputMap.code = menuKeyCode
-            menuKeyCode = nil
-        end
-    end       
+    return inCombat
 end
 
-local function onLoad()
-    timer.start{
-        type = timer.real,
-        duration = 0.5,
-        iterations = -1, 
-        callback = checkInCombat
-    }
+---@param reference tes3reference
+local function getLootableTarget(reference)
+    local isContainer = reference.baseObject.objectType == tes3.objectType.container
+    local isDead = reference.mobile and reference.mobile.isDead
+    return isContainer or isDead
 end
-event.register("loaded", onLoad)
 
 local function onActivate(e)
-    if config.enable then
-        local isContainer = e.target.object.objectType == tes3.objectType.container
-        local isDead = ( 
-            ( e.target.object.objectType == tes3.objectType.npc or e.target.object.objectType == tes3.objectType.creature ) and 
-            e.target.mobile.health.current <= 0
-        )
-        if inCombat and ( isContainer or isDead ) then
-            tes3.messageBox("You are in combat.")
+    if getLootableTarget(e.target) then
+        if checkInCombat() then
+            showInCombatMessage()
             return false
         end
     end
@@ -105,22 +103,81 @@ end
 
 event.register("activate", onActivate)
 
+local function isBlockedButtonPressed(buttonPressed, device)
+    for _, keybind in ipairs(BUTTONS_TO_BLOCK) do
+        local inputBinding = tes3.getInputBinding(keybind)
+        local isButton = buttonPressed == inputBinding.code
+        local isDevice = inputBinding.device == device
+        if isButton and isDevice then
+            return true
+        end
+    end
+    return false
+end
+
+local function getButtonStateData()
+    tes3.player.tempData.noCombatMenuButtonState = tes3.player.tempData.noCombatMenuButtonState or {
+        [DEVICE_KEYBOARD] = {},
+        [DEVICE_MOUSE] = {}
+    }
+    return tes3.player.tempData.noCombatMenuButtonState
+end
+
+local function setButtonPressed(button, device)
+    getButtonStateData()[device][tostring(button)] = true
+end
+
+local function unsetButtonPressed(button, device)
+    getButtonStateData()[device][tostring(button)] = nil
+end
+
+local function getButtonPressed(button, device)
+    return getButtonStateData()[device][tostring(button)] == true
+end
 
 
-local function onMenuButton(e)
-    if config.enable then
-        local showMsg = (
-            e.button == menuKeyCode and
-            menuInputMap.code == substituteKeyCode and
-            not tes3.menuMode()
-        )
-        if showMsg then
-            tes3.messageBox("You are in combat.")
+local function doBlockButton(button, device)
+    logger:debug("%s Button Blocked: %s", button, getDeviceName(device))
+    if device == DEVICE_KEYBOARD then
+        tes3.worldController.inputController.keyboardState[button +1] = 0
+    else
+        tes3.worldController.inputController.mouseState.buttons[button +1] = 0
+    end
+end
+
+local function checkBlockButton(button, device)
+    if not tes3.player then return end
+    if isBlockedButtonPressed(button, device) then
+        logger:debug("ButtonDown: %s, device: %s", button, getDeviceName(device))
+        if getButtonPressed(button, device) then
+            logger:debug("button %s already blocked", button, getDeviceName(device))
+            doBlockButton(button, device)
+        elseif checkInCombat() then
+            showInCombatMessage()
+            setButtonPressed(button, device)
+            doBlockButton(button, device)
+            timer.start{
+                duration = 1.0,
+                callback = function()
+                    unsetButtonPressed(button, device)
+                end
+            }
         end
     end
 end
 
-event.register("mouseButtonDown", onMenuButton)
+---@param e mouseButtonDownEventData
+local function onMouseButtonDown(e)
+    checkBlockButton(e.button, DEVICE_MOUSE)
+end
+event.register("mouseButtonDown", onMouseButtonDown)
+
+
+---@param e keyDownEventData
+local function onKeyDown(e)
+    checkBlockButton(e.keyCode, DEVICE_KEYBOARD)
+end
+event.register("keyDown", onKeyDown)
 
 
 ------------------------------------------------------
@@ -136,7 +193,7 @@ local sidebarDefault = (
 )
 
 local function addSideBar(component)
-    component.sidebar:createInfo{ text = sideBarDefault}
+    component.sidebar:createInfo{ text = sidebarDefault }
     component.sidebar:createHyperLink{
         text = "Made by Merlord",
         exec = "start https://www.nexusmods.com/users/3040468?tab=user+files",
@@ -153,7 +210,7 @@ end
 
 local function makeVar(id, numbersOnly)
     return mwse.mcm.createTableVariable{
-        id = id, 
+        id = id,
         table = config,
         numbersOnly = numbersOnly
     }
@@ -161,7 +218,7 @@ end
 
 local function registerModConfig()
     local template = mwse.mcm.createTemplate("No Combat Menu")
-    template:saveOnClose(configPath, config)
+    template:saveOnClose(CONFIG_PATH, config)
     template:register()
 
     local page = template:createSideBarPage()
@@ -177,13 +234,27 @@ local function registerModConfig()
         label = "Safe Distance",
         description = "The distance from the nearest enemy at which you can access your inventory.",
         variable = makeVar("safeDistance"),
-        min = 0, 
-        max = 4000,
-        jump = 200,
+        min = 0,
+        max = 10000,
+        jump = 1000,
         step = 1
     }
 
-
+    page:createDropdown{
+        label = "Logging Level",
+        description = "Set the log level.",
+        options = {
+            { label = "Trace", value = "TRACE"},
+            { label = "Debug", value = "DEBUG"},
+            { label = "Info", value = "INFO"},
+            { label = "Error", value = "ERROR"},
+            { label = "None", value = "NONE"},
+        },
+        variable = mwse.mcm.createTableVariable{ id = "logLevel", table = config },
+        callback = function(self)
+            logger:setLogLevel(self.variable.value)
+        end
+    }
 end
 
 event.register("modConfigReady", registerModConfig)
